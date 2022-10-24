3c8919cc66b2635493c6dd4d7377f19c08f26710
JonathanStewart2 <Jonathan.stewart2@hotmail.com>
2022-10-24 17:05:31 +0100

    working on READ

--- frontend/src/Components/Portfolio/Portfolio.jsx
+++ frontend/src/Components/Portfolio/Portfolio.jsx
@@ -1,17 +1,26 @@
-
+import { useState } from 'react';
+import axios from 'axios';
 
 
 const Portfolio = () => {
-    
+    const [portfolio, setPortfolio] = useState([]);
 
-
+    const getPortfolio = async () => {
+        const URL = 'http://localhost:4417/getPortfolio/ETH';
+        const response = await axios.get(URL);
+        console.log(response);
+        setPortfolio(response);
+        console.log(portfolio)
+    }
 
     return (
-        <>
-        <button type="button">My Portfolio</button>
-        <button type="button">Add Invesment</button>
-        <button type="button">Sell Investment</button>
-        </>
+        <div>
+            <header>
+                <button type="button" onClick={() => getPortfolio()}>My Portfolio</button>
+                <button type="button">Add Invesment</button>
+                <button type="button">Sell Investment</button>
+            </header>
+        </div>
     )
 }
 
